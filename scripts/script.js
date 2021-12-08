d70168a01ca9f21516a952b4b9ae4195472ce2bc
Rafael Martinez <rafaelmartinez.contato@gmail.com>
2021-12-08 08:29:27 -0300

    começando a estruturar o arquivo script.js com a lógica do jogo

---
+++ scripts/script.js
@@ -0,0 +1,55 @@
+//variável que irá receber a soma dos pontos
+let score = 0;
+
+const questions = [
+    {
+        question:'question 1',
+        answersAlternatives: ['alternative1','alternative2', 'alternative3']
+    },
+    {
+        question:'question 2',
+        answersAlternatives: ['alternative1','alternative2', 'alternative3']
+    },
+    {
+        question:'question 3',
+        answersAlternatives: ['alternative1','alternative2', 'alternative3']
+    },
+    {
+        question:'question 4',
+        answersAlternatives: ['alternative1','alternative2', 'alternative3']
+    },
+    {
+        question:'question 5',
+        answersAlternatives: ['alternative1','alternative2', 'alternative3']
+    },
+    {
+        question:'question 6',
+        answersAlternatives: ['alternative1','alternative2', 'alternative3']
+    },
+    {
+        question:'question 7',
+        answersAlternatives: ['alternative1','alternative2', 'alternative3']
+    },
+    {
+        question:'question 8',
+        answersAlternatives: ['alternative1','alternative2', 'alternative3']
+    },
+    {
+        question:'question 9',
+        answersAlternatives: ['alternative1','alternative2', 'alternative3']
+    },
+    {
+        question:'question 10',
+        answersAlternatives: ['alternative1','alternative2', 'alternative3']
+    },
+];
+
+function checkAnswers(questionsArr) {
+    for (let i = 0; i < questions.length; i++) {
+       
+    }
+}
+
+checkAnswers(questions);
+
+//pensar em algo que vai identificar onde o usuário clicou e definir se a alternativa que ele clicou é onde está a resposta correta
