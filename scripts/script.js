53f08140d98d8a5db548b14679221fca44413801
Rafael Martinez <rafaelmartinez.contato@gmail.com>
2021-12-10 08:22:19 -0300

    Mudei a estrutura do formulário para uma lista usando ul

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,1 @@
-// function getAnswers() {
-//     let answers = document.querySelectorAll('.questionsForm input');
-
-//     for (let i = 0; i < answers.length; i++) {
-//         console.log(answers[i]);
-
-//         if (answers[i].className === 'alternative') {
-            
-//         }
-//     }
-// }
-
-// getAnswers();
+let answers = 
