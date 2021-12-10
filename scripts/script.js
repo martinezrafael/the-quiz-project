a6e580ebcd40885ba560f69030f27ca57c14263d
Rafael Martinez <rafaelmartinez.contato@gmail.com>
2021-12-09 22:27:28 -0300

    criado formulario de perguntas e alterações no layout da página

--- scripts/script.js
+++ scripts/script.js
@@ -1,55 +1,13 @@
-//variável que irá receber a soma dos pontos
-let score = 0;
+// function getAnswers() {
+//     let answers = document.querySelectorAll('.questionsForm input');
 
-const questions = [
-    {
-        question:'question 1',
-        answersAlternatives: ['alternative1','alternative2', 'alternative3']
-    },
-    {
-        question:'question 2',
-        answersAlternatives: ['alternative1','alternative2', 'alternative3']
-    },
-    {
-        question:'question 3',
-        answersAlternatives: ['alternative1','alternative2', 'alternative3']
-    },
-    {
-        question:'question 4',
-        answersAlternatives: ['alternative1','alternative2', 'alternative3']
-    },
-    {
-        question:'question 5',
-        answersAlternatives: ['alternative1','alternative2', 'alternative3']
-    },
-    {
-        question:'question 6',
-        answersAlternatives: ['alternative1','alternative2', 'alternative3']
-    },
-    {
-        question:'question 7',
-        answersAlternatives: ['alternative1','alternative2', 'alternative3']
-    },
-    {
-        question:'question 8',
-        answersAlternatives: ['alternative1','alternative2', 'alternative3']
-    },
-    {
-        question:'question 9',
-        answersAlternatives: ['alternative1','alternative2', 'alternative3']
-    },
-    {
-        question:'question 10',
-        answersAlternatives: ['alternative1','alternative2', 'alternative3']
-    },
-];
+//     for (let i = 0; i < answers.length; i++) {
+//         console.log(answers[i]);
 
-function checkAnswers(questionsArr) {
-    for (let i = 0; i < questions.length; i++) {
-       
-    }
-}
+//         if (answers[i].className === 'alternative') {
+            
+//         }
+//     }
+// }
 
-checkAnswers(questions);
-
-//pensar em algo que vai identificar onde o usuário clicou e definir se a alternativa que ele clicou é onde está a resposta correta
+// getAnswers();
