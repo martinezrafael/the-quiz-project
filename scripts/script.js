8ba6eaec104d4039c4c8fb7dfbea28d79fcdf247
Rafael Martinez <rafaelmartinez.contato@gmail.com>
2021-12-11 16:09:26 -0300

    renderizando o javascript no html

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,24 @@
 //criando array de perguntas
 const questions = [
-  {
-    questionDescription: "Pergunta número 1",
-    answers: ["resposta 1", "resposta 2", "resposta 3"],
-    //aqui indica qual é o índice da reposta correta
-    answerCorrect: 1,
-  },
-  {
-    questionDescription: "Pergunta número 2",
-    answers: ["resposta 1", "resposta 2", "resposta 3"],
-    //aqui indica qual é o índice da reposta correta
-    answerCorrect: 0,
-  },
-  {
-    questionDescription: "Pergunta número 3",
-    answers: ["resposta 1", "resposta 2", "resposta 3"],
-    //aqui indica qual é o índice da reposta correta
-    answerCorrect: 2,
-  },
+    {
+        questionDescription: "Pergunta número 1",
+        answers: ["resposta 1", "resposta 2", "resposta 3"],
+        //aqui indica qual é o índice da reposta correta
+        answerCorrect: 1,
+    },
+    {
+        questionDescription: "Pergunta número 2",
+        answers: ["resposta 1", "resposta 2", "resposta 3"],
+        //aqui indica qual é o índice da reposta correta
+        answerCorrect: 0,
+    },
+    {
+        questionDescription: "Pergunta número 3",
+        answers: ["resposta 1", "resposta 2", "resposta 3"],
+        //aqui indica qual é o índice da reposta correta
+        answerCorrect: 2,
+    }
 ];
-
-
-
 
 //Buscar o elemento body
 const bodyElement = document.getElementsByTagName("body")[0];
@@ -29,13 +26,31 @@ const bodyElement = document.getElementsByTagName("body")[0];
 //criar uma section com a class questions que vai ser a seção de perguntas do site
 const sectionElement = document.createElement("section");
 sectionElement.className = "questions";
+
+//criar uma div com a class questionsContainer onde ficarão todas as perguntas listadas
+const questionsContainer = document.createElement("div");
+questionsContainer.className = "questionsContainer";
+
+
+questions.forEach((question) => {
+    questionsContainer.innerHTML += `<div class='question'>
+        <h3 class='questionDescription'>
+        ${question.questionDescription}
+        </h3>
+        <div class='questionAlternatives'>
+            <ul class='alternativeList'>
+                <li class='alternative'>${question.answers[0]}</li>
+                <li class='alternative'>${question.answers[1]}</li>
+                <li class='alternative'>${question.answers[2]}</li>
+            </ul>
+        </div>
+    </div>`;
+});
+
+sectionElement.appendChild(questionsContainer);
+bodyElement.appendChild(sectionElement);
 // console.log(sectionElement);
 
-//criar uma div com a class questionsContainer onde ficaram todas as perguntas listadas
-const questionsContainerElement = document.createElement('div');
-questionsContainerElement.className = 'questionsContainer';
+const answersList = document.querySelectorAll('.alternative');
 
-//criar uma div com a clas question onde vai ficar armazenada a pergunta individualmente
-const question = document.createElement('div');
-question.className = 'question';
-
+console.log(answersList);
