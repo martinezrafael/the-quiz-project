b72cea24998c75d1536ba73f9b20d4a7e6e15fea
Rafael Martinez <rafaelmartinez.contato@gmail.com>
2021-12-13 20:53:30 -0300

    criei a função que mostra a pontuação na tela e se o jogador venceu ou perdeu

--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,48 @@
-//criando array de perguntas
+//Busca o elemento body
+const bodyElement = document.getElementsByTagName("body")[0];
+
+//cria uma section com a class questions que vai ser a seção de perguntas do site
+const sectionElement = document.createElement("section");
+sectionElement.className = "questions";
+
+//coloca o título e a descrição inicial antes das perguntas
+sectionElement.innerHTML = `<h2 class="title">Bem vindo ao the quiz</h2>
+<p class="initialDescription">Esse é um jogo baseado na newsletter do site <a href="https://thenewscc.com.br/" target="_blank">https://thenewscc.com.br/</a> onde você pode se divertir e se manter atualizado ao mesmo tempo! Boa sorte!</p>`
+
+//cria uma div onde ficarão todas as perguntas listadas
+const questionsContainer = document.createElement("div");
+questionsContainer.className = "questionsContainer";
+
+//coloca a section de perguntas dentro do body
+bodyElement.appendChild(sectionElement);
+
+//coloca o container de perguntas dentro da section de perguntas
+sectionElement.appendChild(questionsContainer);
+
+
+//cria array de perguntas
 const questions = [
     {
         questionDescription: "Pergunta número 1",
         answers: ["resposta 1", "resposta 2", "resposta 3"],
         //aqui indica qual é o índice da reposta correta
-        answerCorrect: 1,
+        answerCorrect: 1 // Resposta 2
     },
     {
         questionDescription: "Pergunta número 2",
         answers: ["resposta 1", "resposta 2", "resposta 3"],
         //aqui indica qual é o índice da reposta correta
-        answerCorrect: 0,
+        answerCorrect: 0 //Resposta 1
     },
     {
         questionDescription: "Pergunta número 3",
         answers: ["resposta 1", "resposta 2", "resposta 3"],
         //aqui indica qual é o índice da reposta correta
-        answerCorrect: 2,
+        answerCorrect: 2 //Resposta 3
     }
 ];
 
-//Buscar o elemento body
-const bodyElement = document.getElementsByTagName("body")[0];
-
-//criar uma section com a class questions que vai ser a seção de perguntas do site
-const sectionElement = document.createElement("section");
-sectionElement.className = "questions";
-
-sectionElement.innerHTML = `<h2 class="title">Bem vindo ao the quiz</h2>
-<p class="initialDescription">Esse é um jogo baseado na newsletter do site <a href="https://thenewscc.com.br/" target="_blank">https://thenewscc.com.br/</a> onde você pode se divertir e se manter atualizado ao mesmo tempo! Boa sorte!</p>`
-
-//criar uma div com a class questionsContainer onde ficarão todas as perguntas listadas
-const questionsContainer = document.createElement("div");
-questionsContainer.className = "questionsContainer";
-
-
+//renderiza as perguntas na tela
 questions.forEach((question) => {
     questionsContainer.innerHTML += `<div class='question'>
         <h3 class='questionDescription'>
@@ -42,18 +50,46 @@ questions.forEach((question) => {
         </h3>
         <div class='questionAlternatives'>
             <ul class='alternativeList'>
-                <li class='alternative'>${question.answers[0]}</li>
-                <li class='alternative'>${question.answers[1]}</li>
-                <li class='alternative'>${question.answers[2]}</li>
+                <li class='alternative' onclick = 'checkAnswer()'>${question.answers[0]}</li>
+                <li class='alternative' onclick = 'checkAnswer()'>${question.answers[1]}</li>
+                <li class='alternative' onclick = 'checkAnswer()'>${question.answers[2]}</li>
             </ul>
         </div>
     </div>`;
 });
 
-sectionElement.appendChild(questionsContainer);
-bodyElement.appendChild(sectionElement);
-// console.log(sectionElement);
 
-const answersList = document.querySelectorAll('.alternative');
 
-console.log(answersList);
+//checa se a resposta clicada é a correta
+function checkAnswer() {
+   
+}
+
+
+let score = 50;
+
+//isso deve ser disparado quando clicar na ultima pergunta
+function showScore () {
+    if (score > 0 && score < 70) {
+
+        let resultContainer = document.createElement('div');
+        resultContainer.innerHTML += `<h3>Você fez ${score} pontos, não foi o suficiente! Tente mais uma vez.</h3>`
+        sectionElement.appendChild(resultContainer);
+    
+    } else if (score <= 100) {
+    
+        let resultContainer = document.createElement('div');
+        resultContainer.innerHTML += `<h3>Parabéns! Você fez ${score} pontos e venceu!!</h3>`
+        sectionElement.appendChild(resultContainer);
+    
+    } else {
+    
+        let resultContainer = document.createElement('div');
+        resultContainer.innerHTML += `<h3>Ocorreu um erro!</h3>`
+        sectionElement.appendChild(resultContainer);
+    
+    }
+}
+
+showScore();
+
