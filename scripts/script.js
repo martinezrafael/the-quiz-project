67dbd885ed05eb9a5efb9a488c0df353cc825ef4
Rafael Martinez <rafaelmartinez.contato@gmail.com>
2021-12-14 22:28:12 -0300

    mudei a renderização e consegui criar o evento de target para identificar a pergunta clicada

--- scripts/script.js
+++ scripts/script.js
@@ -13,12 +13,16 @@ sectionElement.innerHTML = `<h2 class="title">Bem vindo ao the quiz</h2>
 const questionsContainer = document.createElement("div");
 questionsContainer.className = "questionsContainer";
 
+const questionAlternatives = document.createElement("div");
+questionAlternatives.className = "questionAlternatives";
+
 //coloca a section de perguntas dentro do body
 bodyElement.appendChild(sectionElement);
 
 //coloca o container de perguntas dentro da section de perguntas
 sectionElement.appendChild(questionsContainer);
 
+questionsContainer.appendChild(questionAlternatives);
 
 //cria array de perguntas
 const questions = [
@@ -26,54 +30,66 @@ const questions = [
         questionDescription: "Pergunta número 1",
         answers: ["resposta 1", "resposta 2", "resposta 3"],
         //aqui indica qual é o índice da reposta correta
-        answerCorrect: 1 // Resposta 2
+        answerCorrect: 'alternative-0'
     },
     {
         questionDescription: "Pergunta número 2",
-        answers: ["resposta 1", "resposta 2", "resposta 3"],
+        answers: ["resposta 21", "resposta 22", "resposta 23"],
         //aqui indica qual é o índice da reposta correta
-        answerCorrect: 0 //Resposta 1
+        answerCorrect: 'alternative-1'
     },
     {
         questionDescription: "Pergunta número 3",
-        answers: ["resposta 1", "resposta 2", "resposta 3"],
+        answers: ["resposta 31", "resposta 32", "resposta 33"],
         //aqui indica qual é o índice da reposta correta
-        answerCorrect: 2 //Resposta 3
+        answerCorrect: 'alternative-2'
     }
 ];
 
 //renderiza as perguntas na tela
-questions.forEach((question) => {
+const renderAnswers = (answerArray, indexQuestion) => {
+    let alternativeAnswer = ''
+    answerArray.forEach((element, index) =>  alternativeAnswer += `<li class='alternative-${index} indexQuestion-${indexQuestion}' onclick ='checkAnswer(event)'>${element}</li>`)
+    return alternativeAnswer;
+}
+
+questions.forEach((question, index) => {
     questionsContainer.innerHTML += `<div class='question'>
-        <h3 class='questionDescription'>
-        ${question.questionDescription}
-        </h3>
-        <div class='questionAlternatives'>
-            <ul class='alternativeList'>
-                <li class='alternative' onclick = 'checkAnswer()'>${question.answers[0]}</li>
-                <li class='alternative' onclick = 'checkAnswer()'>${question.answers[1]}</li>
-                <li class='alternative' onclick = 'checkAnswer()'>${question.answers[2]}</li>
-            </ul>
-        </div>
-    </div>`;
+        <h3 class='questionDescription'>${question.questionDescription}</h3>
+
+        <ul class='alternativeList'>
+            ${renderAnswers(question.answers, index)}
+        </ul>
+
+    </div>`
+
 });
 
 
+let score = 0;
 
 //checa se a resposta clicada é a correta
-function checkAnswer() {
-   
+function checkAnswer(event) {
+    let targetAnswer = event.target;
+
+    let answerClick = targetAnswer.className.split(' ')[0];
+    console.log(`resposta clicada ${answerClick}`);
+
+    let questionClick = targetAnswer.className.split(' ')[1].split('-')[1];
+    console.log(`Qual a pergunta ${questionClick}`);
+    
+    
+    
+
 }
 
-
-let score = 50;
 
 //isso deve ser disparado quando clicar na ultima pergunta
 function showScore () {
     if (score > 0 && score < 70) {
 
         let resultContainer = document.createElement('div');
-        resultContainer.innerHTML += `<h3>Você fez ${score} pontos, não foi o suficiente! Tente mais uma vez.</h3>`
+        resultContainer.innerHTML += `<h3>Você fez ${score} pontos, não foi o suficiente! Tente outra vez.</h3>`
         sectionElement.appendChild(resultContainer);
     
     } else if (score <= 100) {
@@ -91,5 +107,5 @@ function showScore () {
     }
 }
 
-showScore();
+
 
